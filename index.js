ebffc104d60360483945eb9b3259abe2e21202e6
philipplgh <philipplgh@gmail.com>
2019-05-16 12:39:53 +0200

    add platform filter

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const platform = process.platform === 'win32' ? 'windows' : process.platform
+
 module.exports = {
   name: 'swarm',
   displayName: 'Swarm',
   type: 'storage',
-  repository: 'https://gethstore.blob.core.windows.net'
+  repository: 'https://gethstore.blob.core.windows.net',
+  filter: {
+    name: {
+      includes: [platform]
+    }
+  }
 }
