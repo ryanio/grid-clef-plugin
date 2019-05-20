e8c0f5076bf0012be741b74b60705ceb32e12472
philipplgh <philipplgh@gmail.com>
2019-05-20 10:04:55 +0200

    add settings, and input handling

--- index.js
+++ index.js
@@ -4,10 +4,35 @@ module.exports = {
   name: 'swarm',
   displayName: 'Swarm',
   type: 'storage',
-  repository: 'https://gethstore.blob.core.windows.net',
+  repository: 'https://ethswarmstore.blob.core.windows.net',
   filter: {
     name: {
       includes: [platform]
     }
-  }
+  },
+  settings: [
+    {
+      id: 'bzzaccount',
+      label: 'Bzz Account',
+      flag: '--bzzaccount %s',
+    },
+    {
+      id: 'password',
+      label: 'Account Password',
+      type: 'path',
+      flag: '--password %s'
+    },
+    {
+      id: 'cors',
+      label: 'Cors Domain',
+      default: 'http://localhost:3000',
+      flag: '--corsdomain %s'
+    }
+  ],
+  onInputRequested: (log, handleRequest) => {
+    if (log.startsWith('Passphrase')) {
+      console.log('plugin recognized input request for password')
+      handleRequest('*******')
+    }
+  },
 }
